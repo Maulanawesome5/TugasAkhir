252ae34ab2cd460e036bdb3c734e60609befa62f
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-09 23:24:10 +0700

    Membuat dataframe dari logam dan minyak

--- src/logam.py
+++ src/logam.py
@@ -1,2 +1,14 @@
-alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak'
+import pandas as pd
+
+# path directory
+# alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak'
+gold_data = r'D:\LATIHAN PEMROGRAMAN\(TUGAS AKHIR)\Preprocess Logam dan Minyak\XAU_USD.xlsx'
+silver_data = r'D:\LATIHAN PEMROGRAMAN\(TUGAS AKHIR)\Preprocess Logam dan Minyak\XAG_USD.xlsx'
+
+# dataframe
+gold = pd.read_excel(gold_data)
+silver = pd.read_excel(silver_data)
+
+print(gold)
+print(silver)
 
--- src/minyak.py
+++ src/minyak.py
@@ -1,1 +1,14 @@
-alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak'
+import pandas as pd
+
+# path directory
+# alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak'
+wti_data = r'D:\LATIHAN PEMROGRAMAN\(TUGAS AKHIR)\Preprocess Logam dan Minyak\WTI_USD.xlsx'
+brent_data = r'D:\LATIHAN PEMROGRAMAN\(TUGAS AKHIR)\Preprocess Logam dan Minyak\XBR_USD.xlsx'
+
+# dataframe
+wti = pd.read_excel(wti_data)
+brent = pd.read_excel(brent_data)
+
+print(wti)
+print(brent)
+
