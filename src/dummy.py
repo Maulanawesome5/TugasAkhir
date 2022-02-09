ebd57b971d61a5ee2dc371d04ea1567c84e119a9
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-10 01:45:37 +0700

    Membuat branch untuk uji dummy program

--- src/dummy.py
+++ src/dummy.py
@@ -1,9 +1,24 @@
+import os
 import pandas as pd
 
 # print(gold, silver) # 1322 baris, 1323 baris, 6 kolom
 # print(wti, brent) # 1532 baris, 988 baris, 6 kolom
 
-alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak/csv_format/WTI_USD.csv'
+# alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak/csv_format/WTI_USD.csv'
+# df = pd.read_csv(alamat_data, delimiter=';')
+# print(df)
 
-df = pd.read_csv(alamat_data, delimiter=';')
-print(df)
+alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak/csv_format'
+files = os.listdir(alamat_data)
+
+try:    
+    for file in files:
+        if file.endswith('.csv'):
+            df = pd.read_csv(alamat_data, delimiter=';')
+
+
+except:
+    pass
+
+
+print(files) # hasilnya >> ['WTI_USD.csv', 'XBR_USD.csv']
