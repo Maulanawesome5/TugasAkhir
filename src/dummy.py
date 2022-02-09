8e86c0f491e3b7c94c748aab0d3efb1072955502
Maulanawesome5 <maulanawesome5@gmail.com>
2022-02-10 04:51:02 +0700

    dummy.py : read csv, drop kolom, add prefix, join dataframe, sort asc dataframe, cek NaN value pada dataframe yang sudah di join

--- src/dummy.py
+++ src/dummy.py
@@ -1,13 +1,6 @@
 import os
 import pandas as pd
 
-# print(gold, silver) # 1322 baris, 1323 baris, 6 kolom
-# print(wti, brent) # 1532 baris, 988 baris, 6 kolom
-
-# alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak/csv_format/WTI_USD.csv'
-# df = pd.read_csv(alamat_data, delimiter=';')
-# print(df)
-
 alamat_data = r'D:/LATIHAN PEMROGRAMAN/(TUGAS AKHIR)/Preprocess Logam dan Minyak/csv_format'
 files = os.listdir(alamat_data)
 
@@ -22,4 +15,30 @@ except:
 
 print(files) # hasilnya >> ['WTI_USD.csv', 'XBR_USD.csv']
 
-df = pd.read_csv(alamat_data, usecols=files)
+wti_data = files[0] # tipe data str
+brent_data = files[1]
+
+data1 = pd.read_csv((alamat_data + "/" + wti_data), delimiter=";")
+data2 = pd.read_csv((alamat_data + "/" + brent_data), delimiter=";")
+# print(data1, "\n", data2)
+
+# Mengambil harga closing saja dari kedua dataframe
+data1 = pd.DataFrame(data1).drop(['Open', 'High', 'Low', 'Change %'], axis=1)
+data2 = pd.DataFrame(data2).drop(['Open', 'High', 'Low', 'Change %'], axis=1)
+
+# Menambahkan prefix agar bisa di join antar dataframe
+data1 = pd.DataFrame(data1).add_prefix('WTI_')
+data2 = pd.DataFrame(data2).add_prefix('XBR_')
+
+# Joining kedua dataframe
+df = pd.DataFrame(data1).join(data2).sort_values(by=['WTI_Date', 'XBR_Date'], ascending=True, ignore_index=True)
+print(df)
+
+# Mengecek nilai yang kosong atau NaN pada data
+print(df.isnull().sum()) # XBR_Date (544 NaN), XBR_Price (544 NaN) 
+
+
+
+
+
+
